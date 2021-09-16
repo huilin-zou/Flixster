a3b5dfcf37fe67c42e39ab71abc4e6cfb48a3800
huilin <zouhuilin5@gmail.com>
2021-09-16 17:19:29 -0400

    part 2

--- app/src/main/java/com/codepath/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/flixster/adapters/MovieAdapter.java
@@ -1,20 +1,26 @@
 package com.codepath.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.codepath.flixster.DetailActivity;
 import com.codepath.flixster.R;
 import com.codepath.flixster.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -51,16 +57,20 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        RelativeLayout container;
+
 
         TextView tvTitle;
         TextView tvOverView;
         ImageView ivPoster;
 
         public ViewHolder(@NonNull View itemView) {
+
             super(itemView);
             tvTitle=itemView.findViewById(R.id.tvTitle);
             tvOverView=itemView.findViewById(R.id.tvOverview);
             ivPoster=itemView.findViewById(R.id.ivPoster);
+            container=itemView.findViewById(R.id.container);
 
         }
 
@@ -78,6 +88,14 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             else
                 imageUrl=movie.getPosterPath();
             Glide.with(context).load(imageUrl).into(ivPoster);
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                   Intent i =new Intent(context, DetailActivity.class);
+                   i.putExtra("movie", Parcels.wrap(movie));
+                   context.startActivity(i);
+                }
+            });
 
         }
     }
